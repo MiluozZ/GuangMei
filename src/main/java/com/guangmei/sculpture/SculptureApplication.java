263f78a1dab694a10c90fb9530bd40977718f4a3
luozx <604679490@qq.com>
2022-02-14 17:48:42 +0800

    项目初始化

---
+++ src/main/java/com/guangmei/sculpture/SculptureApplication.java
@@ -0,0 +1,13 @@
+package com.guangmei.sculpture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SculptureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SculptureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guangmei/sculpture/SculptureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guangmei.sculpture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SculptureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
